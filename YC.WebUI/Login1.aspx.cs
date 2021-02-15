e6c6d14e799cb95330ebe582cc42605f4ee4e9ec
Cobe lee <cobe.lee@live.com>
2021-02-15 13:12:00 +0800

    新增

---
+++ YC.WebUI/Login1.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Web.Security;
+public partial class Login1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+       //(TextBox)Login1.FindControl("UserName")
+    }
+}
