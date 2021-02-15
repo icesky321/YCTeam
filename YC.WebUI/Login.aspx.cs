29bd24da8d1d5f1ff5ffbb0dcc66627c2239fdc1
icesky <112410163@qq.com>
2021-02-15 12:45:26 +0800

    用户功能

--- YC.WebUI/Login.aspx.cs
+++ YC.WebUI/Login.aspx.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Web.Security;
 public partial class Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+       //(TextBox)Login1.FindControl("UserName")
     }
 }
